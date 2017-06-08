99a43ae532a28997532b43694dc0fba8ba0119ad
[Arnaud Barbaria] <[arnaud.barbaria@gmail.com]>
2017-06-08 14:45:17 +0200

    get the list of monument

--- app/src/main/java/patrimoine/wcs/fr/toulousemonuments/models/FacetGroup.java
+++ app/src/main/java/patrimoine/wcs/fr/toulousemonuments/models/FacetGroup.java
@@ -1,14 +1,19 @@
 
 package patrimoine.wcs.fr.toulousemonuments.models;
 
+import com.google.api.client.util.Key;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FacetGroup {
 
+    @Key
     private String name;
+    @Key
     private List<Facet> facets = null;
+    @Key
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
     public String getName() {
