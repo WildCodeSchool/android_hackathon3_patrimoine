1c53100b19079388cf0f8851131f7e69996e6825
[Arnaud Barbaria] <[arnaud.barbaria@gmail.com]>
2017-06-09 04:28:06 +0200

    failing loaded high size image

--- app/src/main/java/patrimoine/wcs/fr/toulousemonuments/DescriptionFragment.java
+++ app/src/main/java/patrimoine/wcs/fr/toulousemonuments/DescriptionFragment.java
@@ -2,6 +2,9 @@ package patrimoine.wcs.fr.toulousemonuments;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -15,13 +18,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import patrimoine.wcs.fr.toulousemonuments.models.Fields;
 import patrimoine.wcs.fr.toulousemonuments.models.MonumentModel;
@@ -30,7 +38,12 @@ import patrimoine.wcs.fr.toulousemonuments.models.MonumentModel;
 public class DescriptionFragment extends BaseFragment {
 
     private DatabaseReference mDatabaseReference;
-    private int mScore;
+
+    TextView textViewTitle;
+    ImageView imageViewDescriptionMain;
+    TextView textViewDescription;
+    TextView textViewScore;
+    ImageButton mImageButtonRating;
 
 
     public DescriptionFragment(int position, MonumentModel model) {
@@ -52,13 +65,23 @@ public class DescriptionFragment extends BaseFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Fields currentFields = mMonumentModel.getRecords().get(mPosition).getFields();
-        TelephonyManager telephonyManager = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
 
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child(telephonyManager.getDeviceId());
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child(currentFields.getNomCdt()).child(getContext().getString(R.string.score));
+        textViewTitle = (TextView) getActivity().findViewById(R.id.textViewTitle);
+        imageViewDescriptionMain = (ImageView) getActivity().findViewById(R.id.imageViewDescriptionMain);
+        textViewDescription = (TextView) getActivity().findViewById(R.id.textViewDescription);
+        textViewScore = (TextView) getActivity().findViewById(R.id.textViewScore);
+        mImageButtonRating = (ImageButton) getActivity().findViewById(R.id.imageButtonRating);
+
         mDatabaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                mScore = (int) dataSnapshot.getValue();
+                if (dataSnapshot.getValue(Integer.class) != null) {
+                    textViewScore.setText(String.valueOf(dataSnapshot.getValue(Integer.class)));
+                }
+                else{
+                    textViewScore.setText(String.valueOf(0));
+                }
             }
 
             @Override
@@ -66,20 +89,43 @@ public class DescriptionFragment extends BaseFragment {
 
             }
         });
+        mImageButtonRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabaseReference.runTransaction(new Transaction.Handler() {
+                    @Override
+                    public Transaction.Result doTransaction(MutableData mutableData) {
+                        if (mutableData.getValue() == null){
+                            mutableData.setValue(1);
+                            return Transaction.success(mutableData);
+                        }
+                        int value = mutableData.getValue(Integer.class);
+                        mutableData.setValue(value + 1);
+                        return Transaction.success(mutableData);
+                    }
 
-        TextView textViewTitle = (TextView) getActivity().findViewById(R.id.textViewTitle);
-        ImageView imageViewDescriptionMain = (ImageView) getActivity().findViewById(R.id.imageViewDescriptionMain);
-        TextView textViewDescription = (TextView) getActivity().findViewById(R.id.textViewDescription);
-        ImageButton imageButton = (ImageButton) getActivity().findViewById(R.id.imageButtonRating);
+                    @Override
+                    public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot) {
+
+                    }
+                });
+            }
+        });
 
         textViewTitle.setText(currentFields.getNom());
         textViewDescription.setText(currentFields.getDescriptif());
 
-        AssetManager assetManager = getActivity().getAssets();
+
         try {
-            Glide.with(getContext())
-                    .load(assetManager.open(currentFields.getNomCdt()))
-                    .into(imageViewDescriptionMain);
+            AssetManager assetManager = getActivity().getAssets();
+            InputStream inputStream = assetManager.open(currentFields.getNomCdt() + ".jpg", AssetManager.ACCESS_BUFFER);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            imageViewDescriptionMain.setImageBitmap(bitmap);
+            /*Glide.with(getContext())
+                    .setDefaultRequestOptions(RequestOptions.centerCropTransform())
+                    .asBitmap()
+                    .load(bitmap)
+                    .into(imageViewDescriptionMain);*/
         } catch (IOException e) {
             e.printStackTrace();
         }
